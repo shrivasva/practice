be195b81b23153e0f3757c082a1be03b8badc0d6
shrivasva <viveek.sri@gmail.com>
2022-12-21 14:19:53 +0530

    updating logic for rotating array

--- src/study/search/binary_search/FindInRotatingArray.java
+++ src/study/search/binary_search/FindInRotatingArray.java
@@ -2,7 +2,11 @@ package study.search.binary_search;
 
 public class FindInRotatingArray {
     public static void main(String[] args) {
-        System.out.println(search(new int[]{1,3,5}, 2));
+        System.out.println(search(getNums(), 1));
+    }
+
+    private static int[] getNums() {
+        return new int[]{1,3};
     }
 
     static int search(int[] nums, int target) {
@@ -10,29 +14,26 @@ public class FindInRotatingArray {
         if (target == nums[peak]) {
             return peak;
         }
-        int ans = orderLessBinary(nums, target, 0, peak);
-        if (ans == -1 && peak != nums.length - 1) {
-            ans = orderLessBinary(nums, target, peak + 1, nums.length - 1);
+        if (target <= nums[peak]) {
+            return orderLessBinary(nums, target, 0, peak);
+        }else{
+            return  orderLessBinary(nums, target, peak + 1, nums.length - 1);
         }
-        return ans;
     }
 
     static int findPeak(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
-        while (start <= end) {
+        while (start < end) {
             int mid = start + (end - start) / 2;
-            if (mid == 0) {
-                return mid ;
+
+            if (nums[mid] > nums[mid + 1]) {
+                return mid;
             }
-            if (nums[mid - 1] > nums[mid]) {
-                return mid - 1;
-            }
-            if (nums[mid - 1] < nums[mid]) {
-                start = mid;
-                if(start == mid){
-                    return mid;
-                }
+            if (nums[start] > nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
 
         }
@@ -41,24 +42,15 @@ public class FindInRotatingArray {
     }
 
     private static int orderLessBinary(int[] nums, int target, int start, int end) {
-        boolean isDesc = nums[start] > nums[end];
         while (start <= end) {
             int mid = (start + end) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            if (isDesc) {
-                if (target < nums[mid]) {
-                    start = mid + 1;
-                } else if (target > nums[mid]) {
-                    end = mid - 1;
-                }
-            } else {
-                if (target < nums[mid]) {
-                    end = mid - 1;
-                } else if (target > nums[mid]) {
-                    start = mid + 1;
-                }
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
             }
         }
         return -1;
