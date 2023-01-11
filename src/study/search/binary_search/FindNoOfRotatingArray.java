b8a74990a0098f587cf50dff9bf5e3f1d893a388
shrivasva <viveek.sri@gmail.com>
2023-01-11 17:18:49 +0530

    rotatinf array

--- src/study/search/binary_search/FindNoOfRotatingArray.java
+++ src/study/search/binary_search/FindNoOfRotatingArray.java
@@ -6,12 +6,12 @@ import java.util.Arrays;
 
 public class FindNoOfRotatingArray {
     public static void main(String[] args) {
-        int[] arrs = new int[]{0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 6, 6, 8, 9};
-        for (int i = 0; i < 10; i++) {
+
+        int[] arrs = new int[]{0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 6, 6, 8, 9, 10, 10, 11, 11, 11, 11, 11, 12};
+        for (int i = 0; i < 100; i++) {
             RotateArray.rotateArray(arrs, i);
             System.out.println(Arrays.toString(arrs));
-            System.out.println(search(arrs, 4));
-
+            System.out.println("Element found at " + search(arrs, 4));
         }
     }
 
@@ -20,7 +20,10 @@ public class FindNoOfRotatingArray {
         if (target == nums[peak]) {
             return peak;
         }
-        System.out.println("peak "+peak+" --> " + nums[peak]);
+        System.out.println("Peak " + nums[peak]);
+//        if ((nums[peak] != 12)) {
+//            throw new RuntimeException("INVALID PEAK");
+//        }
         if (nums[0] > target) {
             return orderLessBinary(nums, target, peak + 1, nums.length - 1);
         }
@@ -28,40 +31,29 @@ public class FindNoOfRotatingArray {
 
     }
 
-    static int findPeak(int[] nums) {
+    private static int findPeak(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
-        while (start <= end) {
-            if(start == end || nums[start]>nums[start+1]){
-                return start;
-            }
-            int mid = start + (end - start) / 2;
-            if (mid < end && nums[mid] > nums[mid + 1]) {
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] > nums[mid + 1]) {
                 return mid;
             }
-            if (mid > start && nums[mid] < nums[mid - 1]) {
+            if (nums[mid] < nums[mid - 1]) {
                 return mid - 1;
             }
-            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
-                if (nums[start] > nums[start + 1]) {
-                    return start;
-                }
-                if (nums[end] < nums[end - 1]) {
-                    return end;
-                }
+            if (nums[start] == nums[end] && nums[end] == nums[mid]) {
                 start++;
                 end--;
-            } else if (nums[start] < nums[mid] ||
-                    (nums[start] == nums[mid] && nums[mid] > nums[end]) ||
-                    nums[mid] < nums[end]) {
-                start= mid +1;
-            } else {
-                end= mid -1;
             }
-
+            if (nums[start] > nums[mid]) {
+                end = mid - 1;
+            }
+            if (nums[mid] > nums[start] || nums[mid] < nums[end] || nums[start] == nums[mid]) {
+                start = mid + 1;
+            }
         }
-        return -1;
-
+        return start;
     }
 
     private static int orderLessBinary(int[] nums, int target, int start, int end) {
